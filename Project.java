a96b3db38b4be94a8c54e58f942f184762eff68b
idilsg <64634611+idilsg@users.noreply.github.com>
2022-12-12 22:18:00 +0300

    adding cards array
    it has a wrong line, will fix later

--- Project.java
+++ Project.java
@@ -1,26 +1,41 @@
 package project;
 
-import java.util.Random;
+//import java.util.Random;
 
 public class Project {
   public static void main(String args[]) {
-    int[] exampleArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
-
-    shuffleArray (exampleArray);
-    for (int i = 0; i < exampleArray.length; i++) {
-      System.out.print(exampleArray[i] + " ");
-    }
-    System.out.println();
-  }
-  
-  public static void shuffleArray (int[] arr) {
+     String[] cards = new String[13];
+     for (int i=0; i<10; i++){
+         cards[i+1]= i+2; //wrong line
+     } 
+     
+     cards[0]= "A";
+     cards[10]= "J";
+     cards[11]= "Q";
+     cards[12]= "K";
+     
+      for (int j=0; j<13; j++) {
+          System.out.print(cards[j] + " ");
+      }
       
-    Random r = new Random();
-    for (int i = arr.length - 1; i > 0; i--) {
+      
+/*    int[] exampleArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+      shuffleArray (exampleArray);
+      for (int i = 0; i < exampleArray.length; i++) {
+      System.out.print(exampleArray[i] + " ");
+      }
+      System.out.println();
+      }
+      
+      public static void shuffleArray (int[] arr) {
+      Random r = new Random();
+      for (int i = arr.length - 1; i > 0; i--) {
       int index = r.nextInt(i + 1);
       int a = arr[index];
       arr[index] = arr[i];
       arr[i] = a;
-    } 
+      }
+      
+     */
   } 
 }

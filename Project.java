40cbe534f4e690f984fa9ac51b1d5ed18dbb993e
İdil Sanem Gürsoy <64634611+idilsg@users.noreply.github.com>
2022-12-18 15:06:33 +0300

    fix

--- Project.java
+++ Project.java
@@ -1,12 +1,10 @@
 package project;
 
 import java.util.Random;
-import java.util.Scanner;
 
 public class Project {
 
     public static void main(String[] args) {
-
 
         String[] suits = {"♠", "♣", "♥", "♦"};
         String[] cards = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
@@ -21,35 +19,31 @@ public class Project {
                 x++;
             }
         }
-        
+
         shuffleArray(shuffledDeck);
-        
-        
-        
+
         Random r2 = new Random();
-        int cuttingPoint = r2.nextInt(shuffledDeck.length-2)+1;
-       
-        
-        for (int i=cuttingPoint+1; i<=51; i++) {
-            cutDeck[y] = shuffledDeck[i];
-            y++;
-           
-        }
-        
-        for (int i=0; i<=cuttingPoint; i++) {
+        int cuttingPoint = r2.nextInt(shuffledDeck.length - 2) + 1;
+
+        for (int i = cuttingPoint + 1; i <= 51; i++) {
             cutDeck[y] = shuffledDeck[i];
             y++;
         }
-        
-        for (String element: cutDeck) {
-            System.out.print(element + " ");
+
+        for (int i = 0; i <= cuttingPoint; i++) {
+            cutDeck[y] = shuffledDeck[i];
+            y++;
+        }
+
+        for (String element : cutDeck) {
+            System.out.print(element + " "); //checking
         }
     }
 
     
     public static void shuffleArray(String[] arr) {
         Random r1 = new Random();
-        
+
         for (int i = arr.length - 1; i > 0; i--) {
             int index = r1.nextInt(i + 1);
             String a = arr[index];

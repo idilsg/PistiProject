2e1d94d39ee47213f6f9cef6c3ea53db8e330acb
İdil Sanem Gürsoy <64634611+idilsg@users.noreply.github.com>
2022-12-19 21:19:14 +0300

    starting to dealing the cards

--- Project.java
+++ Project.java
@@ -3,7 +3,31 @@ package project;
 import java.util.Random;
 
 public class Project {
+    
+    public static void shuffleArray(String[] arr) {
+        Random r1 = new Random();
 
+        for (int i = arr.length - 1; i > 0; i--) {
+            int index = r1.nextInt(i + 1);
+            String a = arr[index];
+            arr[index] = arr[i];
+            arr[i] = a;
+        }
+    }
+    
+    public static void dealCards(String[] arr) {
+        String computersCards[] = null;
+        String playersCards[] = null;
+        
+        for (int i=0; i<4; i++) {
+            arr[i] = computersCards[i];
+        }
+        
+        for (int j=0; j<4; j++) {
+            arr[j+4] = playersCards[j];
+        }
+    }
+ 
     public static void main(String[] args) {
 
         String[] suits = {"♠", "♣", "♥", "♦"};
@@ -30,25 +54,31 @@ public class Project {
             y++;
         }
 
-        for (int i = 0; i <= cuttingPoint; i++) {
-            cutDeck[y] = shuffledDeck[i];
+        for (int j = 0; j <= cuttingPoint; j++) {
+            cutDeck[y] = shuffledDeck[j];
             y++;
         }
 
-        for (String element : cutDeck) {
-            System.out.print(element + " "); //checking
+        for (String s : cutDeck) {
+            System.out.print(s + " "); //checking
         }
-    }
-
     
-    public static void shuffleArray(String[] arr) {
-        Random r1 = new Random();
-
-        for (int i = arr.length - 1; i > 0; i--) {
-            int index = r1.nextInt(i + 1);
-            String a = arr[index];
-            arr[index] = arr[i];
-            arr[i] = a;
+        
+        String table[] = null;
+        String computer[] = null;
+        String player[] = null;
+        String remainingCards[] = null;
+    
+        for (int i=0; i<4; i++) {
+            table[i] = cutDeck[i];
+        }
+        
+        for (int j=0; j<cutDeck.length-4; j++) {
+            remainingCards[j] = cutDeck[j+4];
+        }
+    
+        for (String s : remainingCards) {
+            System.out.print(s + " "); //checking
         }
     }
 }

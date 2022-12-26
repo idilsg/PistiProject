d72134eee53c715b2487c46ce028a2ee04e08680
İdil Sanem Gürsoy <64634611+idilsg@users.noreply.github.com>
2022-12-26 22:21:53 +0300

    scores

--- Project.java
+++ Project.java
@@ -4,10 +4,10 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Project {
-
+    
     public static void shuffleArray(String[] arr) {
         Random r1 = new Random();
-
+        
         for (int i = arr.length - 1; i > 0; i--) {
             int index = r1.nextInt(i + 1);
             String a = arr[index];
@@ -15,10 +15,10 @@ public class Project {
             arr[i] = a;
         }
     }
-
+    
     public static void playingCards(String[] arr) {
         Scanner sc = new Scanner(System.in);
-
+        
         if (arr.length == 4) {
             String sequence1 = sc.next();
             switch (sequence1) {
@@ -37,7 +37,7 @@ public class Project {
                 default:
                     System.out.print("You entered invalid sequence. ");
             }
-
+            
             if (arr.length == 3) {
                 String sequence2 = sc.next();
                 switch (sequence2) {
@@ -54,7 +54,7 @@ public class Project {
                         System.out.print("You entered invalid sequence. ");
                 }
             }
-
+            
             if (arr.length == 2) {
                 String sequence3 = sc.next();
                 switch (sequence3) {
@@ -68,7 +68,7 @@ public class Project {
                         System.out.print("You entered invalid sequence. ");
                 }
             }
-
+            
             if (arr.length == 1) {
                 String sequence4 = sc.next();
                 switch (sequence4) {
@@ -81,9 +81,9 @@ public class Project {
             }
         }
     }
-
+    
     public static void main(String[] args) {
-
+        
         String[] suits = {"♠", "♣", "♥", "♦"};
         String[] ranks = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
         String[] deck = new String[52];
@@ -91,49 +91,53 @@ public class Project {
         String[] tableCards = new String[4];
         String[] computersHand = new String[4];
         String[] playersHand = new String[4];
+        int computersTotalCards = 0;
+        int playersTotalCards = 0;
+        String[] computersCards = new String[computersTotalCards];
+        String[] playersCards = new String[playersTotalCards];
         String[] remainingDeck = new String[40];
         int computerScore = 0;
         int playerScore = 0;
         int x = 0;
         int y = 0;
-
+        
         for (String suit : suits) {
             for (String rank : ranks) {
                 deck[x] = suit + rank; //concatenating two arrays
                 x++;
             }
         }
-
+        
         String[] shuffledDeck = deck.clone();
-
+        
         shuffleArray(shuffledDeck);
 
         //cutting the deck
         Random r2 = new Random();
         int cuttingPoint = r2.nextInt(shuffledDeck.length - 1);
-
+        
         for (int i = cuttingPoint + 1; i < 52; i++) {
             cutDeck[y] = shuffledDeck[i];
             y++;
         }
-
+        
         for (int j = 0; j <= cuttingPoint; j++) {
             cutDeck[y] = shuffledDeck[j];
             y++;
         }
-
+        
         for (String s : cutDeck) {
             System.out.print(s + " "); //checking, delete later
         }
-
+        
         System.out.print("\n---\n"); //delete later
         System.out.print("Cards on the table: ");
-
+        
         for (int i = 0; i < tableCards.length; i++) {
             tableCards[i] = cutDeck[i];
             System.out.print(tableCards[i] + " ");
         }
-
+        
         System.out.print("\nComputer's cards: "); //checking, delete later
 
         //computers hand
@@ -141,7 +145,7 @@ public class Project {
             computersHand[i] = cutDeck[i + tableCards.length];
             System.out.print(computersHand[i] + " "); //checking, delete later
         }
-
+        
         System.out.print("\nYour cards: ");
 
         //players hand
@@ -154,16 +158,28 @@ public class Project {
         for (int i = 0; i < remainingDeck.length; i++) {
             remainingDeck[i] = cutDeck[i + tableCards.length + computersHand.length + playersHand.length];
         }
-
+        
         System.out.print("\nEnter the sequence number of the card that you want to play: ");
         playingCards(playersHand);
         
-        if (computerScore > playerScore) {
-            System.out.println("\nComputer wins!");
-        } else if (playerScore > computerScore){
-            System.out.println("\nYou win!");
+        if (playersTotalCards > computersTotalCards) {
+            playerScore += 3;
+        } else if (computersTotalCards > playersTotalCards) {
+            computerScore += 3;
         } else {
-            System.out.println("\nNobody won! Your score and computers score are equal.");
+            playerScore += 0;
+            computerScore += 0;
+        }
+        
+        System.out.print("\nYour score: " + playerScore);
+        System.out.print("\nComputers score: " + computerScore);
+        
+        if (computerScore > playerScore) {
+            System.out.print("\nComputer wins!");
+        } else if (playerScore > computerScore) {
+            System.out.print("\nYou win!");
+        } else {
+            System.out.print("\nNobody won! Your score and computers score are equal.");
         }
     }
 }
